ac30269d51b9de3a765217b074019e2b5f5552e0
Thomas Chang <thomas.sonobe@gmail.com>
2021-05-04 15:02:06 -0300

    Primeiro Componente

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default RepositoryList;
